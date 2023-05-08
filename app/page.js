65f03fd43a43e768e852a097b8c24cc2443b4ccc
HeeChan Yoon <yhc920923@gmail.com>
2023-05-08 14:55:54 +0900

    component, import

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-      <div>
-      </div>
-  )
+    let name = 'park'
+      return (
+          <div>
+              <h4 className="title">애플후레시</h4>
+              <p className="title-sub">by dev { name }</p>
+          </div>
+      )
 }
